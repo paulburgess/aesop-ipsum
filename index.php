a79bdeb5e5280b5f7d04bf2b9a441a829b708dbe
Paul Burgess <paul@delargedesign.com>
2017-09-04 00:31:29 +0100

    Added refresh button

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Lorum ipsum generator using Aesop Rock lyrics</title>
-	<link rel="stylesheet" href="_/css/screen.css?<?php echo substr(md5(rand()), 0, 5); ?>">
+	<link rel="stylesheet" href="_/css/screen.css">
 
 </head>
 <body>
@@ -44,15 +44,28 @@
 	</article>
 
 
+	<div class="refresh">
+
+		<button onclick="javascript:window.location.reload();"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"><g transform="translate(0, 0)"><path fill="#444444" d="M7.5,14C4.5,14,2,11.5,2,8.5S4.5,3,7.5,3c1.2,0,2.2,0.4,3.2,1H8v2h6V0h-2v2.5C10.7,1.5,9.2,1,7.5,1 C3.4,1,0,4.4,0,8.5S3.4,16,7.5,16S15,12.6,15,8.5h-2C13,11.5,10.5,14,7.5,14z"></path></g></svg> Refresh the page for different text</button>
+
+
+	</div>
+
+
 </main>
+
+
+
 
 <footer>
 	<div class="holder">
+
 
 	<p><a href="https://rhymesayers.com/artists/aesoprock">Official Aesop Rock web page on rhymesayers.com</a></p>
 
 	<div class="foot-notes">
 		<p>Based and built on <a href="https://github.com/ryhan/fixie">fixie.js</a></p>
+		<p>Header photo <a href="https://www.flickr.com/photos/delarge/2230759958/in/album-72157603822916205/">by us</a></p>
 		<p>A project from the <a href="https://projects.delargedesign.com/">Delarge web studio</a></p>
 	</div>
 
