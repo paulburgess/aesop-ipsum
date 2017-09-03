f3515856acd209e421a9b0bac68226d49f997964
Paul Burgess <paul@delargedesign.com>
2017-09-03 22:13:02 +0100

    base page layout

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lorum ipsum generator using Aesop Rock lyrics</title>
+	<link rel="stylesheet" href="_/css/screen.css?<?php echo substr(md5(rand()), 0, 5); ?>">
+
+</head>
+<body>
+
+<header role="banner">
+
+</header>
+
+	<main>
+	<article>
+		<h1 class="aesop"></h1>
+
+		<section class="aesop">
+			<p></p>
+			<!-- <img/> -->
+			<ul></ul>
+			<p></p>
+			<ol></ol>
+		</section>
+		<h2 class="aesop"></h2>
+		<section class="aesop"></section>
+
+		<h3 class="aesop"></h3>
+		<section class="aesop"></section>
+
+	</article>
+
+
+</main>
+
+	<script type="text/javascript" src="aesop-ipsum.js?i987g"></script>
+	<script>
+		// Changes default image source to Flickr
+		aesop.setImagePlaceholder('http://farm3.staticflickr.com/2210/2230759958_c4b930b05b.jpg').init();
+	</script>
+</body>
+</html>
