55bdaddf3860c3641250e804ab61ea31e5e41f6c
Paul Burgess <paul@delargedesign.com>
2017-09-03 21:26:59 +0100

    filename chabges and replaces fixie references with aesop

--- aesop-ipsum.js
+++ aesop-ipsum.js
@@ -1,13 +1,13 @@
-/* 
- * Fixie.js
+/*
+ * aesop.js
  * by Ryhan Hassan
  * ryhanh@me.com
  *
  * Automagically adds filler content
- * whenever an element has class="fixie".
+ * whenever an element has class="aesop".
  * Hope you find it useful :)
- */ 
-var fixie = (
+ */
+var aesop = (
 function () {
 
     var selector;
@@ -26,34 +26,34 @@ function () {
         };
     }
 
-    /* 
+    /*
      * Spec
      * Here are some functions you might find useful
-     * 
-     * fixie_handler(element)
-     * fixie_handle_elements(elements)
      *
-     * fixie_fetchWord();
-     * fixie_fetchPhrase();
-     * fixie_fetchLine();
-     * fixie_fetchParagraph();
-     * fixie_fetchParagraphs();
+     * aesop_handler(element)
+     * aesop_handle_elements(elements)
+     *
+     * aesop_fetchWord();
+     * aesop_fetchPhrase();
+     * aesop_fetchLine();
+     * aesop_fetchParagraph();
+     * aesop_fetchParagraphs();
      *
      */
 
 
-    /* 
-     * fixie_handler(element)
+    /*
+     * aesop_handler(element)
      *
      * Takes in an element and adds filler content.
      * Returns false if tag is unrecognized.
      */
-    function fixie_handler(element) {
+    function aesop_handler(element) {
         if (!/^\s*$/.test(element.innerHTML)){
             var childs = element.children;
             if(childs.length){
-                for(var fixie_i = 0; fixie_i < childs.length; fixie_i++){
-                    fixie_handler(childs[fixie_i]);
+                for(var aesop_i = 0; aesop_i < childs.length; aesop_i++){
+                    aesop_handler(childs[aesop_i]);
                 }
             }
             return;
@@ -67,7 +67,7 @@ function () {
         case 'td':
         case 'title':
         case 'tr':
-            element.innerHTML = fixie_fetchWord();
+            element.innerHTML = aesop_fetchWord();
             break;
 
         case 'header':
@@ -89,83 +89,83 @@ function () {
         case 'h4':
         case 'h5':
         case 'h6':
-            element.innerHTML = fixie_fetchPhrase();
+            element.innerHTML = aesop_fetchPhrase();
             break;
 
         case 'footer':
         case 'aside':
         case 'summary':
         case 'blockquote':
-            element.innerHTML = fixie_fetchParagraph();
+            element.innerHTML = aesop_fetchParagraph();
             break;
 
         case 'p':
-            element.innerHTML = fixie_fetchVerse();
+            element.innerHTML = aesop_fetchVerse();
             break;
-            
+
 
         case 'article':
         case 'section':
-            element.innerHTML = fixie_fetchParagraphs()
+            element.innerHTML = aesop_fetchParagraphs()
             break;
 
             /* Special cases */
         case 'a':
             element.href = "http://ryhan.me/";
-            element.innerHTML = "www." + fixie_fetchWord() + fixie_capitalize(fixie_fetchWord()) + ".com";
+            element.innerHTML = "www." + aesop_fetchWord() + aesop_capitalize(aesop_fetchWord()) + ".com";
             break;
 
         case 'img':
             var src = element.getAttribute('src') || element.src;
-            var temp = element.getAttribute('fixie-temp-img');
+            var temp = element.getAttribute('aesop-temp-img');
             if(src == "" || src == null || temp == true || temp == "true"){
                 var width = element.getAttribute('width') || element.width || (element.width = 250);
                 var height = element.getAttribute('height') || element.height || (element.height = 100);
                 var title = element.getAttribute('title') || '';
                 element.src = imagePlaceHolder.replace('${w}', width).replace('${h}', height).replace('${text}', title);
-                element.setAttribute('fixie-temp-img', true);
+                element.setAttribute('aesop-temp-img', true);
             }
             break;
 
         case 'ol':
         case 'ul':
-            element.innerHTML = fixie_fetchList();
+            element.innerHTML = aesop_fetchList();
             break;
 
         case 'dl':
-            element.innerHTML = fixie_fetchDefinitionList();
+            element.innerHTML = aesop_fetchDefinitionList();
             break;
-       
+
         case 'hr':
             break;
 
         default:
-            element.innerHTML = fixie_fetchLine();
+            element.innerHTML = aesop_fetchLine();
         }
     }
 
     // Handle an array of elements
-    function fixie_handle_elements(elements){
+    function aesop_handle_elements(elements){
         for (var i = 0; i < elements.length; i++) {
-            fixie_handler(elements[i]);
+            aesop_handler(elements[i]);
         }
     }
 
 
     // Begin generator
     // PB: Going to start with lines, move to verses for paragraphs later
-    
+
     // PB: Going to need to bring back word library too for a, td etc.
     // e.g. sucker, waders, heavens
-    
+
 
     /////////////////////////////////////////////////
-    
+
     // +++ WORDS
-    
+
     /////////////////////////////////////////////////
 
-    var fixie_wordlibrary = [
+    var aesop_wordlibrary = [
     "flavors",
     "waders",
     "naysayers",
@@ -179,16 +179,16 @@ function () {
     "brain",
     "melody",
     "outside"];
-    
-    
+
+
     /////////////////////////////////////////////////
-    
+
     // +++ LINES
-    
+
     /////////////////////////////////////////////////
-    
-    
-    var fixie_linelibrary = [
+
+
+    var aesop_linelibrary = [
     "Is a love such as that which I exhibit for my practice",
     "Let a sucker drift, I lift up every stone prone to find",
     "And a scent, your riddles yield a little plastic blend",
@@ -203,45 +203,45 @@ function () {
 
 
     /////////////////////////////////////////////////
-    
+
     // +++ VERSES
-    
+
     /////////////////////////////////////////////////
 
 
-    var fixie_verselibrary = [
+    var aesop_verselibrary = [
     "Ok, I lay me down to sleep, creepin' a slumber under red skies, heads splittin', straight sippin' a drip of dead vibes, It's red tides from here, stop and smell analog hell, Clenchin' a stench of burnin' logics and a child with yearning optics",
 
     "Billy-goat beard twenty years in the making, Carried lures in his brim, carried beer in his waders, stinked like alcohol of all prominent flavors, carried knives in his vest, carried war in his nature, sat among the forest floor critters and pine cones, could tie a perfect fly with his eyes closed, Veteran angler with a mission to run, Make all naysayers hold t-t-t-tongues",
- 
+
     "Ease back; let a heart thump echo normalcy for 10, let the back burner boiling point descend, I race the derby in the first heat (strike personal) Strike personal space with the most utterly putrid version of grace",
-    
+
     "Busting accidental dirt bike donuts, outside the most ridiculous poison tongue brain silo, dead before the chubby debutante conquered the high note, schooled by the cruel intention inventions pensive sideshow",
-    
+
     "Earth to AR vertical burden has increased at an alarming rate, bliss is down a point, murder up, glee down and still falling, still crawling out of bed at 2 on Saturdays, came this blind soldier-burning confessional"
-    
-    
+
+
 
 ];
 
 
-    function fixie_capitalize(string) {
+    function aesop_capitalize(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
     ///// ===================================
     ///// ++ Load up the words you need here
-    
-    function fixie_fetchWord() {
-        return fixie_wordlibrary[constrain(0, fixie_wordlibrary.length - 1 )];
-    }
-    
-    function fixie_fetchLineWords() {
-        return fixie_linelibrary[constrain(0, fixie_linelibrary.length - 1 )];
+
+    function aesop_fetchWord() {
+        return aesop_wordlibrary[constrain(0, aesop_wordlibrary.length - 1 )];
     }
 
-  function fixie_fetchVerseWords() {
-        return fixie_verselibrary[constrain(0, fixie_verselibrary.length - 1 )];
+    function aesop_fetchLineWords() {
+        return aesop_linelibrary[constrain(0, aesop_linelibrary.length - 1 )];
+    }
+
+  function aesop_fetchVerseWords() {
+        return aesop_verselibrary[constrain(0, aesop_verselibrary.length - 1 )];
     }
 
      ///// ===================================
@@ -250,74 +250,74 @@ function () {
          return Math.round(Math.random() * (max - min) + min);
     }
 
-    function fixie_fetch(min, max, func, join) {
+    function aesop_fetch(min, max, func, join) {
         join || (join = ' ');
-        var fixie_length = constrain(min, max);
+        var aesop_length = constrain(min, max);
         var result = [];
-        for (var fixie_i = 0; fixie_i < fixie_length; fixie_i++) {
+        for (var aesop_i = 0; aesop_i < aesop_length; aesop_i++) {
             result.push(func());
         }
-        return fixie_capitalize(result.join(join));
+        return aesop_capitalize(result.join(join));
     }
 
     function fetch_suroundWithTag(min, max, func, tagName) {
         var startTag = '<' + tagName + '>';
         var endTag = '</' + tagName + '>';
-        return startTag + fixie_fetch(min, max, func, endTag + startTag) + endTag;
+        return startTag + aesop_fetch(min, max, func, endTag + startTag) + endTag;
     }
 
 
     ///// ===================================
     ///// ++ Get what you need here
 
-     function fixie_fetchLine() {
-        return fixie_fetch(1, 1, fixie_fetchLineWords) + '.';
+     function aesop_fetchLine() {
+        return aesop_fetch(1, 1, aesop_fetchLineWords) + '.';
     }
 
 
-    function fixie_fetchPhrase() {
-        //return fixie_fetch(3, 5, fixie_fetchWord);
-        
+    function aesop_fetchPhrase() {
+        //return aesop_fetch(3, 5, aesop_fetchWord);
+
         // short one liners for header / small elemnents
-        return fixie_fetch(1, 1, fixie_fetchLine);
+        return aesop_fetch(1, 1, aesop_fetchLine);
     }
-    
-    function fixie_fetchVerse() {
-        //return fixie_fetch(3, 5, fixie_fetchWord);
-        
+
+    function aesop_fetchVerse() {
+        //return aesop_fetch(3, 5, aesop_fetchWord);
+
         // short one liners for header / small elemnents
-        return fixie_fetch(1, 1, fixie_fetchVerseWords);
+        return aesop_fetch(1, 1, aesop_fetchVerseWords);
     }
 
-  
 
-    function fixie_fetchParagraph() {
-        return fixie_fetch(1, 1, fixie_fetchLine);
+
+    function aesop_fetchParagraph() {
+        return aesop_fetch(1, 1, aesop_fetchLine);
     }
 
-    function fixie_fetchParagraphs() {
-        return fetch_suroundWithTag(3, 7, fixie_fetchVerse, 'p');
+    function aesop_fetchParagraphs() {
+        return fetch_suroundWithTag(3, 7, aesop_fetchVerse, 'p');
     }
 
-    function fixie_fetchList() {
-        return fetch_suroundWithTag(4, 8, fixie_fetchPhrase, 'li');
+    function aesop_fetchList() {
+        return fetch_suroundWithTag(4, 8, aesop_fetchPhrase, 'li');
     }
 
-    function fixie_fetchDefinitionList() {
+    function aesop_fetchDefinitionList() {
         var html = ''
         for (var i = 0, l = constrain(3,5); i < l; i++) {
-            html += fetch_suroundWithTag(1, 1, fixie_fetchPhrase, 'dt') + fetch_suroundWithTag(1, 1, fixie_fetchPhrase, 'dd');
+            html += fetch_suroundWithTag(1, 1, aesop_fetchPhrase, 'dt') + fetch_suroundWithTag(1, 1, aesop_fetchPhrase, 'dd');
         }
         console.log(html)
         return html;
     }
 
- 
+
      ///// ===================================
- 
-   
-    // Handle all elements with class 'fixie'
-    fixie_handle_elements(document.getElementsByClassName('fixie'));
+
+
+    // Handle all elements with class 'aesop'
+    aesop_handle_elements(document.getElementsByClassName('aesop'));
 
     // Handle elements which match give css selectors
 
@@ -327,9 +327,9 @@ function () {
             return false;
         }
         try {
-            fixie_handle_elements(document.querySelectorAll(selector_str));
+            aesop_handle_elements(document.querySelectorAll(selector_str));
             return true;
-        } 
+        }
         catch (err) {
             return false;
         }
@@ -341,7 +341,7 @@ function () {
             if (selector) {
                 init_str(selector);
             } else {
-                fixie_handle_elements(document.getElementsByClassName('fixie'));
+                aesop_handle_elements(document.getElementsByClassName('aesop'));
             }
         },
         'setImagePlaceholder': function(pl) {
@@ -353,7 +353,7 @@ function () {
                 selector = sl.join(",");
             } else if (sl){
                 selector = sl;
-            } 
+            }
             return this;
         }
     };
